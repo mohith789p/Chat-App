ff7e0a8950689ea2d99391c960261ffbfe369447
mohith789p <mohith321p@gmail.com>
2024-09-07 19:57:35 +0530

    commit 8

--- chatwave/lib/pages/chat_page.dart
+++ chatwave/lib/pages/chat_page.dart
@@ -1,9 +1,33 @@
+import 'package:chatwave/components/text_field.dart';
+import 'package:chatwave/services/auth/auth_service.dart';
+import 'package:chatwave/services/chat/chat_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
   final String receiverEmail;
+  final String receiverID;
+  ChatPage({
+    super.key,
+    required this.receiverEmail,
+    required this.receiverID,
+  });
 
-  const ChatPage({super.key, required this.receiverEmail});
+  // text controller
+  final TextEditingController _messageController = TextEditingController();
+  // chat & auth serrvices
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
+
+  // send message
+  void sendMessage() async {
+    // if message is not empty
+    if (_messageController.text.isNotEmpty) {
+      // send the message
+      await _chatService.sendMessage(receiverID, _messageController.text);
+      _messageController.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +35,69 @@ class ChatPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(receiverEmail),
       ),
+      body: Column(
+        children: [
+          // display all messages
+          Expanded(child: _buildMessageList()),
+          // user input
+          _buildUserInput()
+        ],
+      ),
+    );
+  }
+
+  // build message list
+  Widget _buildMessageList() {
+    String senderID = _authService.getCurrentUser()!.uid;
+    return StreamBuilder(
+      stream: _chatService.getMessages(receiverID, senderID),
+      builder: (context, snapshot) {
+        // error
+        if (snapshot.hasError) {
+          return const Center(child: Text("Error"));
+        }
+        // loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: Text("Loading..."));
+        }
+        // No data
+        if (snapshot.data!.docs.isEmpty) {
+          return const Center(child: Text("No messages"));
+        }
+        // return list view
+        return ListView(
+          children:
+              snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList(),
+        );
+      },
+    );
+  }
+
+  Widget _buildMessageItem(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return Text(data["message"]);
+  }
+
+  // build message input
+  Widget _buildUserInput() {
+    return Row(
+      children: [
+        // textfield should take up most of the space
+        Expanded(
+          child: MyTextField(
+            controller: _messageController,
+            hintText: "Type a message",
+            obscureText: false,
+          ),
+        ),
+        // send button
+        IconButton(
+          onPressed: sendMessage,
+          icon: const Icon(
+            Icons.send,
+          ),
+        ),
+      ],
     );
   }
 }
--- chatwave/lib/services/chat/chat_service.dart
+++ chatwave/lib/services/chat/chat_service.dart
@@ -1,8 +1,11 @@
+import 'package:chatwave/models/message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatService {
-  // get the instance of firestore
+  // get the instance of firestore & auth
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   // get user stream
   Stream<List<Map<String, dynamic>>> getUsersStream() {
     return _firestore.collection("Users").snapshots().map((snapshot) {
@@ -14,6 +17,46 @@ class ChatService {
       }).toList();
     });
   }
+
   // send messages
+  Future<void> sendMessage(String receiverID, String message) async {
+    // get current user
+    final String currentUserID = _auth.currentUser!.uid;
+    final String currentUserEmail = _auth.currentUser!.email!;
+    final Timestamp timestamp = Timestamp.now();
+
+    // create a new message
+    Message newMessage = Message(
+      senderID: currentUserID,
+      senderEmail: currentUserEmail,
+      receiverID: receiverID,
+      message: message,
+      timestamp: timestamp,
+    );
+    // construct a chat room ID for the two users
+    List<String> ids = [currentUserID, receiverID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+    // add new message to database
+    await _firestore
+        .collection("Chat_rooms")
+        .doc(chatRoomID)
+        .collection("messages")
+        .add(newMessage.toMap());
+  }
+
   // get messages
+  Stream<QuerySnapshot> getMessages(String userID, String otherUserID) {
+    // construct a chat room ID for the two users
+    List<String> ids = [userID, otherUserID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+
+    return _firestore
+        .collection("chat_rooms")
+        .doc(chatRoomID)
+        .collection("messages")
+        .orderBy("timestamp", descending: false)
+        .snapshots();
+  }
 }
