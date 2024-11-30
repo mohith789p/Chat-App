7c25428931d4842ef3e06333d0cd76c4f01db667
mohith789p <mohith321p@gmail.com>
2024-11-30 14:23:01 +0530

    update

--- chatwave/lib/pages/chat_page.dart
+++ chatwave/lib/pages/chat_page.dart
@@ -64,8 +64,6 @@ class ChatPage extends StatelessWidget {
         if (snapshot.data!.docs.isEmpty) {
           return const Center(child: Text("No messages"));
         }
-        print("Snapshot data: ${snapshot.data}");
-        print("Snapshot error: ${snapshot.error}");
         // return list view
         return ListView(
           children:
